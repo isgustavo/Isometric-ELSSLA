38f035d5cf6afaff2567a59d3a73b69a478321f8
Gustavo F Oliveira <hi.gustavo.oliveira@gmail.com>
2017-04-05 16:30:47 -0300

    Initial isometric controller

---
+++ Assets/Scripts/ControllerBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ControllerBehaviour : MonoBehaviour {
+
+	[Range(1,9)]
+	public int rotateFactor;
+	[Range(10,30)]
+	public int speedFactor;
+
+	private Rigidbody rb;
+
+
+	void Start () {
+
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		Vector3 oldAngles = this.transform.eulerAngles;
+		#if UNITY_EDITOR
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			this.transform.eulerAngles = new Vector3(oldAngles.x, oldAngles.y + (-rotateFactor), oldAngles.z);
+		} else if (Input.GetKey(KeyCode.RightArrow)) {
+			this.transform.eulerAngles = new Vector3(oldAngles.x, oldAngles.y + (rotateFactor), oldAngles.z);
+		}
+			
+		#else 
+		float tiltValue = GetTiltValue();
+		this.transform.eulerAngles = new Vector3(oldAngles.x, oldAngles.y, oldAngles.z + (tiltValue * ROTATE_AMOUNT));
+		#endif
+
+	}
+
+	void FixedUpdate () {
+
+		#if UNITY_EDITOR
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			rb.AddForce (transform.forward * speedFactor, ForceMode.Acceleration);
+		}
+		#endif
+	}
+}
---
+++ Assets/Scripts/GameCameraBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCameraBehaviour : MonoBehaviour {
+
+	private Vector3 offset;
+	private Transform target;
+
+	void Start () {
+
+		GameObject player = GameObject.FindGameObjectWithTag ("Player");
+		if (player != null) {
+
+			target = player.transform;
+			offset = transform.position - target.position;
+
+		} else {
+
+			Debug.LogError("Cant found player object");
+			gameObject.SetActive (false);
+		}
+	}
+
+	void LateUpdate () {
+
+		transform.position = target.position + offset;
+	}
+}
