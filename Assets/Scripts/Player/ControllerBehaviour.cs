60458312aa3349cdc43b683e66ac76bd0d70d64f
Gustavo F Oliveira <hi.gustavo.oliveira@gmail.com>
2017-04-07 16:01:54 -0300

    Add isometric player controller

---
+++ Assets/Scripts/Player/ControllerBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ControllerBehaviour : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	void Start () {
+
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void Update () {
+
+		gameObject.transform.rotation = Quaternion.AngleAxis(RotationJoystick.instance.GetAngle (), Vector3.up);
+	}
+		
+	void FixedUpdate () {
+
+
+	}
+}
