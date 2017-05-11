4ad3663c90570b930e183b1c52320e787ca77090
Gustavo F Oliveira <hi.gustavo.oliveira@gmail.com>
2017-05-11 19:38:10 -0300

    Add Pop up content

---
+++ Assets/Scripts/GUI/Main/SettingBehaviour.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Facebook.Unity;
+
+public class SettingBehaviour : MonoBehaviour {
+
+
+	[SerializeField]
+	private GameObject _popUp;
+	[SerializeField]
+	private Animator _animator;
+
+	void Start () {
+
+		_popUp.SetActive (false);
+
+	}
+
+	public void OpenSetting () {
+
+		_popUp.SetActive (true);
+		_animator.Play ("StartAnimation");
+
+	}
+
+	public void CloseSetting () {
+
+		StartCoroutine (CloseCoroutine ());
+
+	}
+
+
+	IEnumerator CloseCoroutine () {
+
+
+		_animator.SetTrigger ("Close");
+		yield return new WaitForSeconds (15);
+		_popUp.SetActive (false);
+
+	}
+
+
+
+
+	public void FacebookLogout () {
+
+		if (FB.IsLoggedIn) {
+			FB.LogOut ();
+		}
+	}
+
+
+
+}
